293a4f491e7956f386d75ed756dc53c877dec8bc
ashley-lu <ashleyjlu@g.ucla.edu>
2022-06-05 16:44:48 -0700

    add map on load and fix cards

--- js/init.js
+++ js/init.js
@@ -1,9 +1,12 @@
-// declare variables
-let mapOptions = { 'center': [34.0689, -118.4452], scrollWheelZoom: false, 'zoom': 5 };
-// remove map scroll
+// 1) Popup doesn't open on flyto
+// 2) Does it make sense to have no scroll on the map at all, or should I enable scrolling on only the portion of the map containing the cards
+// 3) Can I shrink the marker size to some default size when I zoom out (so that the large UCLA marker isn't misleading)
 
-// let onCampus = L.featureGroup();
-// let offCampus = L.featureGroup();
+// declare variables
+let mapOptions = { 'center': [34.0689, -118.4452], 'zoom': 5 };
+
+let takesPrEP = L.featureGroup();
+let doesNotTakePrEP = L.featureGroup();
 
 // let layers = {
 //     "On Campus PrEP Access": onCampus,
@@ -17,12 +20,21 @@ let mapOptions = { 'center': [34.0689, -118.4452], scrollWheelZoom: false, 'zoom
 // make UCLA marker larger the more responses there are (but only for non-PrEP users)
 
 let circleOptions = {
-    radius: 4,
-    fillColor: "#ff7800",
-    color: "red",
-    weight: 1,
+    radius: 10,
+    weight: 3,
     opacity: 1,
-    fillOpacity: 0.8
+    fillOpacity: 0.2,
+    color: "blue"
+};
+
+let noResponses = 0;
+
+let circleOptionsUCLA = {
+    radius: 10,
+    weight: 3,
+    opacity: 1,
+    fillOpacity: 0.05,
+    color: "red"
 };
 
 // keep track of values for chart
@@ -31,7 +43,7 @@ let lackprep = 0;
 
 const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSe3tKw51PzIC6dhUv5W8DEqg77o-usnZuTLBdgddFiD9rm3edq-Qa0pqJyOlSxnVtWEmR0YT--5Vmy/pub?output=csv";
 
-const map = L.map('theMap').setView(mapOptions.center, mapOptions.zoom);
+const map = L.map('theMap', {scrollWheelZoom: false}).setView(mapOptions.center, mapOptions.zoom);
 
 let Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
     attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
@@ -59,11 +71,25 @@ function count(data) {
 //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 // }).addTo(map);
 
+yesMarkers = []
+noMarkers = []
+
 function addMarker(data) {
     if (data['Do you take PrEP (pre-exposure prophylaxis) right now?'] == "Yes") {
-        circleOptions.fillColor = "red"
-        L.circleMarker([data.lat, data.lng], circleOptions).bindPopup(`<h2>` + data['address title'] + `</h2>`).addTo(map);
+        circleOptions.fillColor = "blue"
+        marker = L.circleMarker([data.lat, data.lng], circleOptions).bindPopup(`<h2>` + data['address title'] + `</h2>`)
+        takesPrEP.addLayer(marker);
         // createButtons(data.lat, data.lng, data['What address do you go to in order to access PrEP?'])
+        yesMarkers.push(marker);
+    }
+
+    else {
+        circleOptionsUCLA.fillColor = "red"
+        circleOptionsUCLA.radius = 40 * noResponses;
+        //console.log(noResponses)
+        marker = L.circleMarker([34.0689, -118.4452], circleOptionsUCLA).bindPopup(`<h2>` + "UCLA" + `</h2>`)
+        doesNotTakePrEP.addLayer(marker);
+        noMarkers.push(marker)
     }
 
     // else if (data['Do you take PrEP (pre-exposure prophylaxis) right now?'] == "No") {
@@ -115,11 +141,13 @@ function loadData(url) {
     })
 };
 
-function createCard(data) {
+function createCard(data, number) {
     const newCard = document.createElement("card");
 
+    newCard.id = "card" + number;
+
     if (data["Do you take PrEP (pre-exposure prophylaxis) right now?"] == "Yes") {
-        newCard.innerHTML = `<div class="card">
+        newCard.innerHTML = `<div class="card" style="cursor: pointer">
 
         <header class="card-header">
           <h1>Header</h1>
@@ -137,7 +165,12 @@ function createCard(data) {
         newCard.setAttribute("lat", data.lat); // sets the latitude 
         newCard.setAttribute("lng", data.lng); // sets the longitude 
         newCard.addEventListener('click', function () {
+        id = newCard.id.replace("card", "")
+        console.log("id: " + id)
         map.flyTo([data.lat, data.lng]); //this is the flyTo from Leaflet
+        console.log(yesMarkers[id])
+        yesMarkers[id].openPopup();
+        
     })
         const spaceForCards = document.getElementById('yes-stories')
         spaceForCards.appendChild(newCard);
@@ -165,11 +198,26 @@ function createCard(data) {
 
 function processData(results) {
     console.log(results)
+
+    results.data.forEach(data => {
+        if (data['Do you take PrEP (pre-exposure prophylaxis) right now?'] == "No")
+        noResponses += 1;
+    })
+
+    let idNumber = 0;
+
     results.data.forEach(data => {
         console.log(data)
         addMarker(data)
-        createCard(data)
+        createCard(data, idNumber)
+        idNumber += 1;
     })
+    
+    // for (let i = 0; i < results.data.length; i++) {
+    //     console.log(results.data)
+    //     addMarker(results.data)
+    //     createCard(results.data, i)
+    //   }
     // onCampus.addTo(map) // add our layers after markers have been made
     // offCampus.addTo(map) // add our layers after markers have been made  
     // let allLayers = L.featureGroup([onCampus, offCampus]);
@@ -219,15 +267,18 @@ canvas.onclick = function (evt) {
         var label = chartData.labels[idx];
         var value = chartData.datasets[0].data[idx];
         console.log(label + " " + value);
-        document.getElementById("instructions").style.display = "none";
         if (label == "Yes") {
             document.getElementById("no-stories").style.display = "none";
-            document.getElementById("right-content").style.display = "block";
+            document.getElementById("yes-stories").style.display = "block";
             map.invalidateSize();
+            takesPrEP.addTo(map);
+            map.fitBounds(takesPrEP.getBounds());
         }
         else if (label == "No") {
-            document.getElementById("right-content").style.display = "none";
+            document.getElementById("yes-stories").style.display = "none";
             document.getElementById("no-stories").style.display = "block";
+            doesNotTakePrEP.addTo(map);
+            map.fitBounds(doesNotTakePrEP.getBounds());
         }
     }
 };
